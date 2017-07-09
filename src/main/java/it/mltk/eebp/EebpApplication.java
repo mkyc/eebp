ff09015a07d97f4603a9779dd97bb077f2008905
Mateusz Kyc <mateusz.kyc@gmail.com>
2017-07-09 19:34:15 +0200

    basic project structure

---
+++ src/main/java/it/mltk/eebp/EebpApplication.java
@@ -0,0 +1,12 @@
+package it.mltk.eebp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EebpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EebpApplication.class, args);
+	}
+}
---
+++ src/test/java/it/mltk/eebp/EebpApplicationTests.java
@@ -0,0 +1,16 @@
+package it.mltk.eebp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EebpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
